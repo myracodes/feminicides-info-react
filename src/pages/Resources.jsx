936d69719b312e59d571a044ac5f29f02cc0c8e3
fmmyriam <benhmyriam@gmail.com>
2021-03-26 15:41:19 +0100

    updated resources page

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -4,32 +4,38 @@ const Resources = () => {
   return (
     <div>
       <h1>Ressources</h1>
-      <h2>Cartes par année</h2>
+      <h2>Historique féminicides en France par année</h2>
       <ul className="cartesParAnnee">
         <li>
-          <a href="https://www.google.com/maps/d/u/1/viewer?hl=fr&mid=1AoTHJTAvWz-P1ddCKe1NhWtdsOQgcLPA&ll=50.45705082693985%2C18.134848058083932&z=3&fbclid=IwAR2-jJGcwvBFX91wDKM7ThWJMF1bqaUv-iV_mnwAlCpZfZnR6OX3jjqFJzs">
+          <a href="https://www.google.com/maps/d/u/1/viewer?hl=fr&mid=1AoTHJTAvWz-P1ddCKe1NhWtdsOQgcLPA&ll=50.45705082693985%2C18.134848058083932&z=3&fbclid=IwAR2-jJGcwvBFX91wDKM7ThWJMF1bqaUv-iV_mnwAlCpZfZnR6OX3jjqFJzs" target="_blank">
             2021</a>
         </li>
         <li>
-          <a href="https://www.google.com/maps/d/u/0/viewer?mid=1KlTxeXvul_y_lfo0JuTdnuksqA1LW6JS&ll=20.78613373268215%2C-72.02392537146807&z=3">
+          <a href="https://www.google.com/maps/d/u/0/viewer?mid=1KlTxeXvul_y_lfo0JuTdnuksqA1LW6JS&ll=20.78613373268215%2C-72.02392537146807&z=3" target="_blank">
             2020</a>
         </li>
         <li>
-          <a href="https://www.google.com/maps/d/u/0/viewer?mid=1Y9bFj8Cjfl3rKwuyDBB5-LNkdKKAjtq9&ll=18.015497665816977%2C0&z=2">
+          <a href="https://www.google.com/maps/d/u/0/viewer?mid=1Y9bFj8Cjfl3rKwuyDBB5-LNkdKKAjtq9&ll=18.015497665816977%2C0&z=2" target="_blank">
             2019</a>
         </li>
         <li>
-          <a href="https://www.google.fr/maps/d/u/0/viewer?mid=19gV1RSgQ5LNG51BeE-WiV7G8m1MocfXZ&ll=-3.81666561775622e-14%2C-69.01567619999992&z=1">
+          <a href="https://www.google.fr/maps/d/u/0/viewer?mid=19gV1RSgQ5LNG51BeE-WiV7G8m1MocfXZ&ll=-3.81666561775622e-14%2C-69.01567619999992&z=1" target="_blank">
             2018</a>
         </li>
         <li>
-          <a href="https://www.google.com/maps/d/u/0/viewer?mid=15ZxvoBUgO4ttolUFSoU5UmuRe98&ll=46.09722818637166%2C3.7726595062499815&z=5">
+          <a href="https://www.google.com/maps/d/u/0/viewer?mid=15ZxvoBUgO4ttolUFSoU5UmuRe98&ll=46.09722818637166%2C3.7726595062499815&z=5" target="_blank">
             2017</a>
         </li>
       </ul>
-      <hr/>
+      <hr />
+      <h2>Arrêtons les violences</h2>
+      <a href="https://arretonslesviolences.gouv.fr/" target="_blank">Arrêtons les violences</a> <br/>
+      <a href="https://arretonslesviolences.gouv.fr/besoin-d-aide" target="_blank">Besoin d'aide</a> <br/>
+      <a href="https://arretonslesviolences.gouv.fr/associations-de-lutte-contre-les-violences-sexistes-et-sexuelles/associations" target="_blank">Trouver une association près de chez vous</a> <br/>
+      <a href="https://arretonslesviolences.gouv.fr/associations-de-lutte-contre-les-violences-sexistes-et-sexuelles#les_associations_nationales1" target="_blank">Les associations nationales</a> <br/>
+      <hr />
       <h2>Haut Conseil à l'Égalité entre les Femmes et les Hommes</h2>
-      <a href="https://www.haut-conseil-egalite.gouv.fr/violences-de-genre/">Violences de genre</a>
+      <a href="https://www.haut-conseil-egalite.gouv.fr/violences-de-genre/" target="_blank">Violences de genre</a> <br/>
     </div>
   )
 }
