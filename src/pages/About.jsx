c9ce4f3520258f71572f3a5f79cc38100e35b270
fmmyriam <benhmyriam@gmail.com>
2021-03-26 15:50:49 +0100

    updated links with noreferrer relationship

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h1>À propos</h1>
       <div>
         Ce site internet est basé sur le travail du collectif Féminicides par compagnons ou ex, qui répertorie et dénonce les féminicides commis en France dans le cadre conjugal. <br/>
-        La partie technique a été réalisée par 4 développeuses : Anaïs Engler, Anna Lefour, Mélusine Rey, et <a href="https://github.com/myracodes">Myriam Mira</a>. {/* Je mets nos liens GitHub ou LinkedIn ou autre site/portfolio de votre choix ?*/}
+        La partie technique a été réalisée par 4 développeuses : Anaïs Engler, Anna Lefour, Mélusine Rey, et <a href="https://github.com/myracodes" target="_blank" rel="noreferrer">Myriam Mira</a>. {/* Je mets nos liens GitHub ou LinkedIn ou autre site/portfolio de votre choix ?*/}
       </div>
     </div>
   )
