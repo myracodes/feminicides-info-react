050c5d3e363a18dba5a9924371cbc8460d5771ef
Anna Lefour <anna.lefour@laposte.net>
2021-03-29 23:18:57 +0200

    Relationship chart done + patternomaly installed

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-// import Data from "./pages/Data";
-import DataComponents from "./pages/DataComponents";
+import Data from "./pages/Data";
 import Map from "./pages/Map";
 import Resources from "./pages/Resources";
 import EditUser from "./pages/Users/EditUser";
@@ -24,8 +23,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/carte" component={Map} />
-        {/* <Route exact path="/donnees" component={Data} /> */}
-        <Route exact path="/donnees" component={DataComponents} />
+        <Route exact path="/donnees" component={Data} />
         <Route exact path="/ressources" component={Resources} />
         <Route exact path="/a-propos" component={About} />
         <Route exact path="/contact" component={Contact} />
---
+++ src/components/Charts-components/RelationshipChart.jsx
@@ -0,0 +1,86 @@
+import React, { PureComponent } from "react";
+import apiHandler from "../../api/apiHandler";
+import pattern from "patternomaly"
+const Chart = require("chart.js");
+
+
+export class relationshipChart extends PureComponent {
+  state = {
+    relationship: [],
+  };
+
+  node = React.createRef(null);
+  chart = React.createRef(null);
+
+  componentDidMount() {
+    apiHandler
+      .dataAllEvents()
+      .then((events) => {
+        let killerStatus = events.map((event) => {
+          return event.relationship;
+        });
+
+        let compagnon = killerStatus.filter((value) => value === "compagnon")
+          .length;
+        let exCompagnon = killerStatus.filter(
+          (value) => value === "ex-compagnon"
+        ).length;
+        let compagnonSup = killerStatus.filter(
+          (value) => value === "compagnon supposé"
+        ).length;
+        let nonRens = killerStatus.filter((value) => value === "non renseigné")
+          .length;
+        let total = compagnon + exCompagnon + compagnonSup + nonRens;
+
+        this.setState({
+          relationship: [compagnon, exCompagnon, compagnonSup, nonRens],
+        });
+
+        let relationshipChart = new Chart(this.node.current, {
+          type: "doughnut",
+          data: {
+            labels: [
+              "Compagnon",
+              "Ex-compagnon",
+              "Compagnon supposé",
+              "Non renseigné",
+            ],
+            datasets: [
+              {
+                label: "Relation avec la victime",
+                data: this.state.relationship,
+                backgroundColor: pattern.generate(["lightgreen", "blue", "red", "grey"]),
+              },
+            ],
+          },
+          options: {
+            title: {
+              display: true,
+              text: `Amount of events : ${total}`,
+            },
+            legend: {
+              display: true,
+              labels: {
+                fontColor: "black",
+              },
+            },
+          },
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    if (this.state.relationship === []) {
+      return <div>Chargement en cours...</div>;
+    }
+
+    return (
+      <div>
+        <canvas style={{ width: 800, height: 300 }} ref={this.node} />
+      </div>
+    );
+  }
+}
+
+export default relationshipChart;
