091f6af0d19144ac8eb678b486682f929dd2af38
Anna Lefour <anna.lefour@laposte.net>
2021-03-29 15:10:44 +0200

    Starting with charts values

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,24 +1,46 @@
-import React, { Component } from 'react';
+import apiHandler from "../api/apiHandler";
+import React, { Component } from "react";
+
 const Chart = require("chart.js");
 
 class Data extends React.Component {
   constructor(props) {
     super(props);
   }
-  
-  state = {
-    victimsAgeData: [1, 50, 70, 80, 90, 100, 77, 88],
-    killerAgeData: [1, 50, 70, 80, 90, 100, 77, 88],
-  }
 
+  state = {
+    victimsAgeData: [],
+    killerAgeData: [1, 50, 70, 80, 90, 100, 77, 88],
+  };
 
   componentDidMount() {
     const node = this.node;
 
+    apiHandler
+      .dataAllEvents()
+      .then((events) => {
+        let eachAge = events.map((event) => event.age);
+        let sortedAges = eachAge.sort();
+
+        
+        
+        this.setState({ victimsAgeData: eachAge });
+      })
+      .catch((err) => console.log(err));
+
     let victimsAgeChart = new Chart(node, {
       type: "bar",
       data: {
-        labels: ["25 ans ou moins", "26 à 35 ans", "36 à 45 ans", "46 à 55 ans", "56 à 65 ans", "66 à 75 ans", "76 à 85 ans", "86 ans et plus"],
+        labels: [
+          "25 ans ou moins",
+          "26 à 35 ans",
+          "36 à 45 ans",
+          "46 à 55 ans",
+          "56 à 65 ans",
+          "66 à 75 ans",
+          "76 à 85 ans",
+          "86 ans et plus",
+        ],
         datasets: [
           {
             label: "Âge des victimes",
@@ -31,8 +53,8 @@ class Data extends React.Component {
               "rgba(107, 255, 99, 0.2)",
               "rgba(107, 255, 99, 0.2)",
               "rgba(107, 255, 99, 0.2)",
-              "rgba(107, 255, 99, 0.2)"
-            ]
+              "rgba(107, 255, 99, 0.2)",
+            ],
           },
           {
             label: "Âge des assassins ou assassins présumés",
@@ -45,20 +67,27 @@ class Data extends React.Component {
               "rgba(255, 99, 99, 0.2)",
               "rgba(255, 99, 99, 0.2)",
               "rgba(255, 99, 99, 0.2)",
-              "rgba(255, 99, 99, 0.2)"
-            ]
-          }
-        ]
-      }
+              "rgba(255, 99, 99, 0.2)",
+            ],
+          },
+        ],
+      },
     });
   }
 
   render() {
+
+    if (this.state.victimsAgeData === []) {
+      return <div>Chargement en cours...</div>
+    }
+
+    console.log('current state', this.state.victimsAgeData)
+
     return (
       <div>
         <canvas
           style={{ width: 800, height: 300 }}
-          ref={node => (this.node = node)}
+          ref={(node) => (this.node = node)}
         />
       </div>
     );
