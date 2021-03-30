e034a1ee8b4b0e4bc3d7fd8e95d186433c6fb575
Anna Lefour <anna.lefour@laposte.net>
2021-03-30 14:45:39 +0200

    Number of events displayed on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import TotalNumber from "../components/Home-component/TotalNumber"
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Home Page ∆</h1>
+        <h1>Home Page</h1>
+        <TotalNumber/>
       </div>
     );
   }
