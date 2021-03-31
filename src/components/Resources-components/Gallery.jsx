213535746c7947cd565b531e3728433a623aba64
fmmyriam <benhmyriam@gmail.com>
2021-03-31 15:41:04 +0200

    basic css done one ressources and galerie pages

--- src/components/Resources-components/Gallery.jsx
+++ src/components/Resources-components/Gallery.jsx
@@ -3,29 +3,29 @@ import apiHandler from './../../api/apiHandler';
 
 export class Gallery extends Component {
 
-constructor(props) {
-    super(props)
-}
+    constructor(props) {
+        super(props)
+    }
 
     state = {
         events: [],
         pictureIndex: 0,
-        imagePath : ""
+        imagePath: ""
     };
 
 
-componentDidMount() {
-    apiHandler
-      .allEvents()
-      .then(data => this.setState({ events: data, pictureIndex: 0 }))
-      .catch(error => console.log(error));
-}
+    componentDidMount() {
+        apiHandler
+            .allEvents()
+            .then(data => this.setState({ events: data, pictureIndex: 0 }))
+            .catch(error => console.log(error));
+    }
 
     previousPicture = (event) => {
         console.log(this.state.events[this.state.pictureIndex].commemoration)
         const currentIndex = this.state.pictureIndex === 0
-                    ? this.state.events.length - 1
-                    : this.state.pictureIndex - 1;
+            ? this.state.events.length - 1
+            : this.state.pictureIndex - 1;
 
         console.log("current index: ", currentIndex);
         this.setState({
@@ -36,8 +36,8 @@ componentDidMount() {
 
     nextPicture = (event) => {
         const currentIndex = this.state.pictureIndex === this.state.events.length - 1
-        ? 0
-        : this.state.pictureIndex + 1;
+            ? 0
+            : this.state.pictureIndex + 1;
         console.log("current index: ", currentIndex);
         this.setState({
             pictureIndex: currentIndex,
@@ -49,25 +49,37 @@ componentDidMount() {
 
     render() {
 
-        if (this.state.events === [] || this.state.events ===null ) {
+        if (this.state.events === [] || this.state.events === null) {
             return (
                 <div>Merci de patienter pendant le chargement...</div>
             )
         }
 
         return (
-            <div>
-                <h1>Galerie photo</h1>
+            <div className="flex flex-column">
+                <h1 className="title-1">Galerie photo</h1>
 
                 {/* {if (this.state.imagePath === "") {
                     return (
                         <div><img src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.6435-9/134188995_201395188317135_7367171530112152291_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=1dUd6ZdF9bIAX9n59OY&_nc_ht=scontent-cdt1-1.xx&oh=2504fa9e947ca7a174da7a84bb325d93&oe=60899739" alt="stop-feminicides-collage-amazone"/></div>
                     )
                 }} */}
-                <button onClick={this.previousPicture}>Précédente</button> | <button onClick={this.nextPicture}>Suivante</button>
-                <div><img src={this.state.imagePath} alt="photo" />
-                Crédit photo : {this.state.imageSource}</div>
-                
+                <div className="gallery flex flex-row gallery-container padding-top">
+                    <span>
+                        <button onClick={this.previousPicture} className="btn-3">←</button>
+                    </span>
+                    <span className="flex flex-column">
+                        <img src={this.state.imagePath} alt="photo" />
+                        <span>
+                            Crédit photo : {this.state.imageSource}
+                        </span>
+                    </span>
+                    <span>
+                        <button onClick={this.nextPicture} className="btn-3">→</button>
+                    </span>
+
+                </div>
+
             </div>
         )
     }
