a2de6dea8a1af1b9eec7fa727e389ad1387fcb9a
fmmyriam <benhmyriam@gmail.com>
2021-03-31 16:25:15 +0200

    about and resources and navmain updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,13 +5,21 @@ const About = () => {
     <div className="page-container">
       <h1 className="title-1">À propos</h1>
       <div className="padding-top">
-        Ce site internet est basé sur le travail du collectif Féminicides par compagnons ou ex, qui répertorie et dénonce les féminicides commis en France dans le cadre conjugal. <br/>
-        La partie technique a été réalisée par 4 développeuses : Anaïs Engler, Anna Lefour, Mélusine Rey, et <a href="https://github.com/myracodes" target="_blank" rel="noreferrer">Myriam Mira</a>. {/* Je mets nos liens GitHub ou LinkedIn ou autre site/portfolio de votre choix ?*/}
+        <div className="padding-top">
+          Ce site internet est basé sur le travail du collectif Féminicides par compagnons ou ex, qui répertorie et dénonce les féminicides commis en France dans le cadre conjugal. <br />
+        </div>
+        <div className="padding-top">
+          La partie technique a été réalisée par 4 développeuses : <br/>
+          <a href="https://github.com/anachannn" target="_blank" rel="noreferrer">Anaïs Engler</a>,<br/>
+          <a href="https://github.com/zelumine" target="_blank" rel="noreferrer">Mélusine Rey</a>,<br/>
+          <a href="https://github.com/ankapieka" target="_blank" rel="noreferrer">Anna Lefour</a>,<br/>
+          et <a href="https://github.com/myracodes" target="_blank" rel="noreferrer">Myriam Mira</a>. <br/>
+        </div>
       </div>
 
       <h1 className="title-1">Contact</h1>
       <div className="padding-top flex flex-column">
-        <a href="mailto:address@e.mail">Contacter le collectif FPCE <br/>
+        <a href="mailto:address@e.mail">Contacter le collectif FPCE <br />
         (Féminicides par compagnons ou ex)</a>
         <a href="https://www.facebook.com/feminicide" target="_blank" rel="noreferrer">Facebook</a>
         <a href="https://www.instagram.com/feminicidespar/" target="_blank" rel="noreferrer">Instagram</a>
