10ea7bba28cec14dd929d0ca6449eb4381824abe
Anna Lefour <anna.lefour@laposte.net>
2021-04-01 12:59:56 +0200

    Home page number in map ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import TotalNumber from "../components/Total-Events/TotalNumber"
-import "./../styles/global.css"
-import "./../styles/Home.css"
+import TotalNumber from "../components/Total-Events/TotalNumber";
+import "./../styles/global.css";
+import "./../styles/Home.css";
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="flex">
+      <div className="homepage-canva">
+        <div>
+          <div>Text</div>
 
-        <div>Text</div>
+          <div className="map-num-container">
+            <div className="home-total-num fade-in">
+              <TotalNumber />
+            </div>
 
-      <div className="flex">
-
-        <div className="absolute">
-        <TotalNumber />
+            <img
+              className="home-map"
+              src="https://res.cloudinary.com/dcbzfldni/image/upload/v1617270285/mapFrance_tlaq0d.png"
+              alt="carte hompage"
+            />
+          </div>
         </div>
-      
-        <img className="relative" src="https://res.cloudinary.com/dcbzfldni/image/upload/v1617270285/mapFrance_tlaq0d.png" alt="carte hompage"/>
-      </div>
-      
-
-       
       </div>
     );
   }
